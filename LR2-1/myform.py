05576096044096b29b51c14bec6a25bc8b07b114
Mary_cpp <f13xxx@rasengan.ru>
2021-04-19 14:29:06 +0300

    Adding empty fields check

--- LR2-1/myform.py
+++ LR2-1/myform.py
@@ -5,3 +5,9 @@ def my_form():
     mail = request.forms.get('ADRESS')
     return "Thanks! The answer will be sent to the mail %s" % mail
 
+def check():
+    mail = request.forms.get('ADRESS')
+    question = request.forms.get('QUEST')
+    if (mail == "" | question == ""):
+        return "Please, fill down the form fields."
+    regex = ""
