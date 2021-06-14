4113eca78137a7a6c2924e732eac550883f6ad30
Maria_Soproniuk <f13x@rasengan.ru>
2021-06-14 18:49:20 +0300

    Form data now an be written into a JSON-object file

--- LR2-1/myform.py
+++ LR2-1/myform.py
@@ -3,27 +3,24 @@ import re
 import pdb
 import json
 
+questions = {}
+questions['question'] = []
+
 @post('/home', method='post')
 def my_form():
     return check()
 
 def check():
-    regex = r'\w+\@\w+\.\w[2-3]'
-    answer = " "
+    regex = re.compile(r'\w+@\w+\.\w')
     mail = request.forms.get('ADRESS')
     question = request.forms.get('QUEST')
-    questions = {}
-    questions['people'] = []
-    questions['people'].append({
-        'mail' : mail,
-        'question' : question})
-    with open('data.txt', 'w') as outfile:
-        json.dump(questions, outfile)
-    answer = "Thanks! The answer will be sent to the mail %s" % mail
-    return answer
-#if (len(mail) == 0  | len(question) == 0):
-        #answer = "Please, fill down the form fields."
-    #elif (mail != re.search(regex, mail)):
-        #answer = "You've entered an incorrect email. Please try again with your email or create a new one"
-    #else:
-        #answer = "Thanks! The answer will be sent to the mail %s" % mail
+    if (len(mail) == 0  | len(question) == 0):
+        return "Please, fill down the form fields."
+    elif regex.match(mail):
+        questions['question'].append({'mail' : mail, 'question' : question})
+        with open('data.txt', 'w') as outfile:
+            json.dump(questions, outfile)
+        return "Thanks! The answer will be sent to the mail %s" % mail
+    else:
+        return "You've entered an incorrect email. Please try again with your email or create a new one"
+        
