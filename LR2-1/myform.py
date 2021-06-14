56121cc863afd7b0c2c86309be834e598b2943cf
Maria_Soproniuk <f13x@rasengan.ru>
2021-06-14 14:48:08 +0300

    Writing dictionary values into JSON file

--- LR2-1/myform.py
+++ LR2-1/myform.py
@@ -1,6 +1,7 @@
 from bottle import post, request
 import re
 import pdb
+import json
 
 @post('/home', method='post')
 def my_form():
@@ -8,17 +9,21 @@ def my_form():
 
 def check():
     regex = r'\w+\@\w+\.\w[2-3]'
+    answer = " "
     mail = request.forms.get('ADRESS')
     question = request.forms.get('QUEST')
     questions = {}
-    answer = " "
-    #if (len(mail) == 0  | len(question) == 0):
+    questions['people'] = []
+    questions['people'].append({
+        'mail' : mail,
+        'question' : question})
+    with open('data.txt', 'w') as outfile:
+        json.dump(questions, outfile)
+    answer = "Thanks! The answer will be sent to the mail %s" % mail
+    return answer
+#if (len(mail) == 0  | len(question) == 0):
         #answer = "Please, fill down the form fields."
     #elif (mail != re.search(regex, mail)):
         #answer = "You've entered an incorrect email. Please try again with your email or create a new one"
     #else:
         #answer = "Thanks! The answer will be sent to the mail %s" % mail
-    questions = {mail : question}
-    pdb.set_trace()
-    answer = "Thanks! The answer will be sent to the mail %s" % mail
-    return answer
